7278dd6c391d378df1c7e6a4820e69feade96ff5
Ruby Labausa <rlabausa@users.noreply.github.com>
2018-12-02 14:50:43 -0600

    Update insertDB.py

--- PythonFiles/insertDB.py
+++ PythonFiles/insertDB.py
@@ -6,22 +6,24 @@ Created on Nov 17, 2018
 import serial
 import time
 import pymongo
+import datetime
+
+
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["testdb"]
 mycol = mydb["testdata"]
 
-port = "/dev/cu.usbmodem412" # configure port based on pc settings
+port = "/dev/cu.usbmodem642" # configure port based on pc settings... run command: ls /dev/cu.* on mac
 baud = 115200
 s = serial.Serial(port)
 s.baudrate = baud
 
-counter = 0
 while True:
+    now = datetime.datetime.now()
 
     data = s.readline() # data is initially read in as a byte
     data = int(data[0:len(data)]) # cast the byte into an integer 
-    mydict = { "temp": data, "time": "12:00AM", "date": "12-18-2018", "counter": counter }
+    mydict = { "data": data, "timestamp": str(now)} #data will be populated by information received from Team Sensing/Network
     x = mycol.insert_one(mydict)
 
-    counter+=1
